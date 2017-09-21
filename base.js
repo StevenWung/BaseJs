4779c865f89645d36c57ce6ce9e90aa1edf3c61b
shaobo.wang <shaobo.wang@veritas.com>
2017-09-21 17:52:36 +0800

    init

---
+++ base.js
@@ -0,0 +1,42 @@
+var BaseJs = BaseJs || {};
+BaseJs.Http = function (options) {
+};
+BaseJs.Http.prototype = {
+    request: function (method, url, data, callback, failure) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var resp = this.responseText;
+                if (callback != undefined)
+                    callback(resp);
+            } else {
+                if (failure != undefined)
+                    failure(this.responseText);
+            }
+        };
+        var httpMethod = method.toLowerCase() == 'POST' ? 'POST' : 'GET';
+        xhttp.open(httpMethod, url, true);
+        xhttp.send();
+    }
+};
+BaseJs.Ajax = function (options) {
+    this.http = new BaseJs.Http();
+    this.options = options || {};
+
+};
+BaseJs.Ajax.prototype = {
+    get: function (url, callback) {
+        this.http.request('GET', url, null, this.options['callback'] || function () {
+        });
+    },
+    post: function (url, data, callback) {
+        this.http.request('POST', url, data, this.options['callback'] || function () {
+        });
+    }
+};
+BaseJs.AjaxRender = function (options) {
+
+};
+BaseJs.AjaxRender.prototype = {
+
+};
