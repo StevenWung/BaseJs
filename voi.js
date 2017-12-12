24cd39d3ee1b9a419fdc70b706e89bd84d6819bb
stevenwung <147852>
2017-12-12 16:42:20 +0800

    basejs

--- voi.js
+++ voi.js
@@ -3,6 +3,7 @@ window.voi = (function (_w, _d) {
     function randomInt() {
         return parseInt(Math.random() * 100000);
     }
+
     _w._ = function (d) {
         if ((new RegExp(/#/g)).test(d)) {
             //id
@@ -20,6 +21,22 @@ window.voi = (function (_w, _d) {
     _w._log = function (d) {
         console.log(d);
     };
+    _w.getJsonData = function (schema, data) {
+        var temp = data;
+        schema.split('.').forEach(function (t, m, n) {
+            if (t.indexOf('[') !== -1) {
+                var res = t.split(/\[|\]/);
+                if (res.length) {
+                    var d = res[0], i = parseInt(res[1]);
+                    temp = temp[d][i]
+                }
+            } else {
+                temp = temp[t];
+            }
+            //console.log(temp);
+        });
+        return temp;
+    }
     var Voi = Voi || {};
     Voi = function () {
 
@@ -39,7 +56,13 @@ window.voi = (function (_w, _d) {
                             if (typeof resp === 'object') {
                                 callback(resp);
                             } else {
-                                callback(JSON.parse(resp));
+                                try {
+                                    resp = JSON.parse(resp)
+
+                                }catch (e){
+                                    console.log(e);
+                                }
+                                callback(resp);
                             }
                         }
 
@@ -52,7 +75,7 @@ window.voi = (function (_w, _d) {
                 xhttp.open(httpMethod, url, true);
                 xhttp.setRequestHeader("AJAX-FORWORD-BASEPHP", "ENCODED-FOR-BASE");
                 this.headers.forEach(function (k) {
-                    for(var i in k){
+                    for (var i in k) {
                         xhttp.setRequestHeader(i, k[i]);
                     }
                 });
@@ -69,18 +92,25 @@ window.voi = (function (_w, _d) {
             if (!options['url'])
                 throw("Url must be set!");
 
+            this.handleResponse = function (resp) {
+                var alert = alert_sys_call || alert;
+                var callback = options['callback']||function(){};
+                var cmd = resp.cmd;
+                if (cmd === 'alert'){
+                    alert(resp.msg)
+                }
+                callback(resp);
+            }
 
         }
 
 
         Ajax.prototype = {
             get: function (param) {
-                this.http.request('GET', this.options['url'], null, this.options['callback'] || function () {
-                    });
+                this.http.request('GET', this.options['url'], null, this.handleResponse);
             },
             post: function (data) {
-                this.http.request('POST', this.options['url'], data, this.options['callback'] || function () {
-                    });
+                this.http.request('POST', this.options['url'], data, this.handleResponse);
             },
             setContentType: function (k) {
                 if (k === 'form')
@@ -110,7 +140,7 @@ window.voi = (function (_w, _d) {
                     var r = resp, rev_r = null;
                     respSchema.split('.').forEach(function (t, m, n) {
                         r = r[t];
-                        if (m + 1 == n.length - 1)
+                        if (m + 1 === n.length - 1)
                             rev_r = r;
                     });
 
@@ -150,14 +180,15 @@ window.voi = (function (_w, _d) {
                     });
                     //dom.style.display = 'none';
                     var pager = rev_r.page;
-                    if (pager && $this.options.pager){
+                    if (pager && $this.options.pager) {
                         var d = _d.getElementById($this.pagerid)
-                        if(!d)
+                        if (!d)
                             d = _d.createElement('div');
                         while (d.hasChildNodes()) {
                             d.removeChild(d.lastChild);
                         }
-                        var page = pager.page, all = pager.all, pagesize = pager.size, pagecount = pager.pagecount, html = '';
+                        var page = pager.page, all = pager.all, pagesize = pager.size, pagecount = pager.pagecount,
+                            html = '';
                         for (var i = 0; i < pagecount; i++) {
                             var a = _d.createElement('a');
                             a.onclick = function () {
@@ -208,32 +239,47 @@ window.voi = (function (_w, _d) {
             this.voi = voi;
             this.ele = ele;
             this.id = 'form_' + randomInt();
+            this.complete = function(){};
         }
 
         XForm.prototype.init = function () {
             //this.voi.log(this.ele);
+            var self = this;
             var ds = this.ele.getAttribute('datasource'), $this = this;
             this.sc = this.ele.getAttribute('schema') || 'result';
+            this.complete = this.ele.getAttribute('complete') || 'complete';
             this.action = this.ele.getAttribute('action');
             if (ds) {
                 (new Voi.Ajax({
                     'url': ds,
                     'callback': function (resp) {
                         var r = resp;
-                        $this.sc.split('.').forEach(function (t) {
+                        /*$this.sc.split('.').forEach(function (t) {
                             r = r[t];
                         });
                         if (Array.isArray(r)) {
                             r = r[0];
                         }
+                        */
+                        r = _w.getJsonData($this.sc, resp);
+                        window[self.complete](r);
                         var keys = Object.keys(r);
                         keys.forEach(function (k) {
-                            var v = r[k], f = $this.ele.querySelector('input[name=' + k + ']');
+                            var v = r[k], f;
+                            f = $this.ele.querySelector('input[name=' + k + ']');
                             if (f)
                                 f.setAttribute('value', v);
+
                             f = $this.ele.querySelector('textarea[name=' + k + ']');
-                            if (f)
+                            if (f){
+                                if (f.className.indexOf('richedit') > -1 && f.nextElementSibling && f.nextElementSibling.tagName === 'iframe'){
+                                    var iframe = f.nextElementSibling;
+                                    iframe.contentWindow.document.write(v);
+
+                                }
                                 f.value = v;
+                            }
+
                         })
 
                     }
@@ -270,21 +316,22 @@ window.voi = (function (_w, _d) {
             this.datasource = this.ele.getAttribute('datasource');
             this.schema = this.ele.getAttribute('schema') || 'result';
         }
-        XBind.prototype.parseHtml = function(html){
+
+        XBind.prototype.parseHtml = function (html) {
             var ele = this.parser.parseFromString(html, "text/html");
             var child = ele.documentElement.querySelector('body').firstChild;
             return child;
         };
-        XBind.prototype.getJsonValue = function(schema, data){
+        XBind.prototype.getJsonValue = function (schema, data) {
             var temp = data;
             schema.split('.').forEach(function (t, m, n) {
-                if (t.indexOf('[') !==-1){
+                if (t.indexOf('[') !== -1) {
                     var res = t.split(/\[|\]/);
-                    if (res.length){
+                    if (res.length) {
                         var d = res[0], i = parseInt(res[1]);
                         temp = temp[d][i]
                     }
-                }else{
+                } else {
                     temp = temp[t];
                 }
                 //console.log(temp);
@@ -293,7 +340,7 @@ window.voi = (function (_w, _d) {
         };
         XBind.prototype.init = function () {
             var $this = this;
-            if (this.ele.length ){
+            if (this.ele.length) {
                 this.ele = this.ele[0];
             }
             this.ele.style.display = 'none';
@@ -313,10 +360,10 @@ window.voi = (function (_w, _d) {
                         var key = t1.replace(/\[|\]/g, '');
                         var val = xbindData[key];
                         html = html.replace(t1, val)
-                    })
-                    var dom = $this.parseHtml(html)
+                    });
+                    var dom = $this.parseHtml(html);
                     dom.style.display = 'block';
-                    $this.ele.replaceWith(dom)
+                    $this.ele.replaceWith(dom);
                     console.log(dom);
 
                 }
@@ -326,20 +373,20 @@ window.voi = (function (_w, _d) {
         XBind.prototype.parseRepeater = function (ele, data) {
             //console.log(ele, data)
             var html = ele.outerHTML, $this = this;
-            var regs = html.match(new RegExp('\\[\\[([a-z|A-Z|0-9|-|_]+)\\]\\]', 'ig'))
+            var regs = html.match(new RegExp('\\[\\[([a-z|A-Z|0-9|-|_]+)\\]\\]', 'ig'));
             ele.style.display = 'none';
-            if (data){
+            if (data) {
                 data.forEach(function (p1, p2, p3) {
                     var tmpHtml = html, dom;
                     regs.forEach(function (t1, t2, t3) {
                         var key = t1.replace(/\[|\]/g, ''), val = '';
-                        if(key === 'key')
+                        if (key === 'key')
                             val = key;
                         else
                             val = p1[key];
                         tmpHtml = tmpHtml.replace(t1, val)
-                    })
-                    dom = $this.parseHtml(tmpHtml)
+                    });
+                    dom = $this.parseHtml(tmpHtml);
                     //ele.parentNode.appendChild(dom)
                     ele.parentNode.insertBefore(dom, ele)
                 })
@@ -351,7 +398,8 @@ window.voi = (function (_w, _d) {
         render: function (__) {
             var $this = this;
             if (!__.isvoi)return;
-            var iterators = __.body.querySelectorAll('[iterator]');
+            var iterators = __.body.querySelectorAll('iterator') || [];
+            //var iterators = iterators1.length ? Array.prototype.concat.call(iterators1,iterators2): iterators2;
             Array.from(iterators).forEach(function (t) {
                 (new Voi.Iterator(__, t)).init();
             });
